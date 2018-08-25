9390e60c898740eb217693d968331ca0be9a0539
Nelson Campos <ncamposfh@gmail.com>
2018-08-25 11:01:01 -0300

    promisse all

--- app.js
+++ app.js
@@ -9,9 +9,25 @@ const getControllers = function(pasta){
 
     return new Promise((resolve, reject) => {
        
-        buscador.buscador("./src", function(err, data){
+        buscador.buscador(pasta, function(err, data){
             if(err){
-                reject()
+                reject(err)
+            } else {
+                resolve(data)
+            }
+           
+        });
+
+    }) 
+}
+
+const getTestes = function(pasta){
+
+    return new Promise((resolve, reject) => {
+       
+        buscador.buscador(pasta, function(err, data){
+            if(err){
+                reject(err)
             } else {
                 resolve(data)
             }
@@ -23,11 +39,24 @@ const getControllers = function(pasta){
 
 
 
-getControllers('./src')
-    .then(arquivos => console.log(arquivos))
+// getControllers('./src')
+//     .then(arquivos => console.log(arquivos))
+//     .catch(err => console.log(err))
 
 
 
+const promises = [
+    getControllers('./src'),
+    getTestes('./tests')
+  ];
+
+
+  Promise.all(promises)
+    .then(data => {
+      let arquivos = data;  
+      console.log("Dados", JSON.stringify(arquivos));
+      return data;
+    });
 
 
 
